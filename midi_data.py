dd7944f5cd3fead16af85ae7d25efb3885506105
juham58 <59977027+juham58@users.noreply.github.com>
2021-12-14 16:08:10 -0500

    midi player

---
+++ midi_data.py
@@ -0,0 +1,42 @@
+# programme pour jouer une piece midi
+import pygame
+from mido import MidiFile
+from pathlib import Path
+
+mid = MidiFile(Path.cwd()/"musicnet_midis"/"Ravel"/"2178_gr_rqtf2.mid")
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    if i == 1:
+        for msg in track:
+            print(msg)
+
+def play_music(midi_filename):
+  '''Stream music_file in a blocking manner'''
+  clock = pygame.time.Clock()
+  pygame.mixer.music.load(midi_filename)
+  pygame.mixer.music.play()
+  while pygame.mixer.music.get_busy():
+    clock.tick(30) # check if playback has finished
+    
+midi_filename = Path.cwd()/"musicnet_midis"/"Ravel"/"2178_gr_rqtf2.mid"
+
+# mixer config
+freq = 44100  # audio CD quality
+bitsize = -16   # unsigned 16 bit
+channels = 2  # 1 is mono, 2 is stereo
+buffer = 1024   # number of samples
+pygame.mixer.init(freq, bitsize, channels, buffer)
+
+# optional volume 0 to 1.0
+pygame.mixer.music.set_volume(0.8)
+
+# listen for interruptions
+try:
+  # use the midi file you just saved
+  play_music(midi_filename)
+except KeyboardInterrupt:
+  # if user hits Ctrl/C then exit
+  # (works only in console mode)
+  pygame.mixer.music.fadeout(1000)
+  pygame.mixer.music.stop()
+  raise SystemExit
