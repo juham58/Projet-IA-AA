30925ca5d419394b2534481f1c2ba67bce30e88b
Justin Hamel <59977027+juham58@users.noreply.github.com>
2021-12-16 16:49:56 -0500

    labels

--- creer_labels_instruments.py
+++ creer_labels_instruments.py
@@ -32,13 +32,19 @@ instruments = pd.DataFrame(columns=["ID", "Instruments"])
 dir = Path.cwd()/"musicnet"/"labels"
 files = dir.glob("*.csv")
 instruments = np.array([]).reshape(0, 11)
+instruments_csv = pd.DataFrame(columns=["ID", "Instruments"])
 for file in files:
+    liste_instruments = []
     array_instruments = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     data = pd.read_csv(file)
     for instrument in data["instrument"]:
+        if instrument not in liste_instruments:
+            liste_instruments.append(instrument)
         array_instruments = instrument_vers_label(instrument, array_instruments)
+    liste_instruments = liste_instruments.sort()
     Id = str(file)[-8:-4]
     instruments = np.vstack((instruments, array_instruments))
+    instruments_csv = instruments_csv.append({"ID": int(Id), "Instruments": liste_instruments}, ignore_index=True)
 np.save(Path.cwd()/"musicnet"/"instrument_labels", instruments)
-#instruments.set_index("ID", inplace=True)
-#instruments.to_csv(Path.cwd()/"musicnet"/"instrument_labels.csv")
+instruments_csv.set_index("ID", inplace=True)
+instruments_csv.to_csv(Path.cwd()/"musicnet"/"instrument_labels.csv")
