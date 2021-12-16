2975622b60d95597253f3b3cdbccaabe04f63c59
Justin Hamel <59977027+juham58@users.noreply.github.com>
2021-12-15 22:58:32 -0500

    génération de npy domaine de Fourier plus essaie avec mlp

--- creer_labels_instruments.py
+++ creer_labels_instruments.py
@@ -1,18 +1,44 @@
 # créer les labels d'instruments pour chaque pièce
 import pandas as pd
+import numpy as np
 from pathlib import Path
+
+def instrument_vers_label(instrument, labels):
+    if instrument == 1:
+        labels[0] = 1
+    if instrument == 7:
+        labels[1] = 1
+    if instrument == 41:
+        labels[2] = 1
+    if instrument == 42:
+        labels[3] = 1
+    if instrument == 43:
+        labels[4] = 1
+    if instrument == 44:
+        labels[5] = 1
+    if instrument == 61:
+        labels[6] = 1
+    if instrument == 69:
+        labels[7] = 1
+    if instrument == 71:
+        labels[8] = 1
+    if instrument == 72:
+        labels[9] = 1
+    if instrument == 74:
+        labels[10] = 1
+    return labels
 
 instruments = pd.DataFrame(columns=["ID", "Instruments"])
 dir = Path.cwd()/"musicnet"/"labels"
 files = dir.glob("*.csv")
+instruments = np.array([]).reshape(0, 11)
 for file in files:
-    liste_instruments = []
+    array_instruments = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     data = pd.read_csv(file)
     for instrument in data["instrument"]:
-        if instrument not in liste_instruments:
-            liste_instruments.append(instrument)
-    id = int(str(file)[-8:-4])
-    instruments = instruments.append({"ID": int(str(file)[-8:-4]), "Instruments": liste_instruments}, ignore_index=True)
-
-instruments.set_index("ID", inplace=True)
-instruments.to_csv(Path.cwd()/"musicnet"/"instrument_labels.csv")
+        array_instruments = instrument_vers_label(instrument, array_instruments)
+    Id = str(file)[-8:-4]
+    instruments = np.vstack((instruments, array_instruments))
+np.save(Path.cwd()/"musicnet"/"instrument_labels", instruments)
+#instruments.set_index("ID", inplace=True)
+#instruments.to_csv(Path.cwd()/"musicnet"/"instrument_labels.csv")
---
+++ pretraitement_data.py
@@ -0,0 +1,28 @@
+import pickle
+import pandas as pd
+import numpy as np
+import librosa
+from pathlib import Path
+from tqdm import tqdm
+
+
+print("Prétraitement du dataset")
+dir = Path.cwd()/"musicnet"/"data"
+files = dir.glob("*.wav")
+data = []
+taille_max = 0
+labels = pd.read_csv(Path.cwd()/"musicnet"/"instrument_labels.csv")
+for file in tqdm(files):
+    x, sr = librosa.load(file)
+    X = librosa.stft(x)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    data.append(Xdb)
+    if Xdb.shape[1] > taille_max:
+        taille_max = Xdb.shape[1]
+print("Padding+sauvegarde")
+for i, file in enumerate(data):
+    nom = labels.iloc[i]["ID"]
+    file = np.pad(file, ((0, 0), (0, taille_max-file.shape[1])), "empty")
+    np.save(Path.cwd()/"musicnet"/"data"/str(nom), file)
+
+print("Fin du prétraitement")
